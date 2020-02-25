832a56ec558698fca6a9bc0aeac53fd752f2d13d
phmoll <philipp.moll@aau.at>
2020-02-25 16:09:34 +0100

    Routing works for all except one case

--- minindn/helpers/ip_routing_helper.py
+++ minindn/helpers/ip_routing_helper.py
@@ -24,6 +24,7 @@
 from igraph import Graph
 from mininet.log import info
 
+
 class LinkInfo(object):
     """
     This class is used to encapsule link information (IP and interface names).
@@ -34,6 +35,7 @@ class LinkInfo(object):
         self.start_intf_ip = start_ip
         self.end_intf_name = end_intf_name
         self.end_ip = end_ip
+
 
 class IPRoutingHelper(object):
     """The routing helper allows to run IP-based evaluations with Mini-NDN. It configures static IP
@@ -62,6 +64,27 @@ class IPRoutingHelper(object):
         return None
 
     @staticmethod
+    def calculateAllSubPaths(path):
+        paths = []
+
+        # Append original path and reversed path
+        paths.append(path)
+        reversed = path[:]
+        reversed.reverse()
+        paths.append(reversed)
+
+        # All possible lenghts
+        for l in range(2, len(path)):
+            # All possible sublists of lenght l
+            for i in range(0, len(path) - l + 1):
+                subpath = path[i: i + l]
+                paths.append(subpath)
+                subpath = subpath[:]
+                subpath.reverse()
+                paths.append(subpath)
+        return paths
+
+    @staticmethod
     def calcAllRoutes(net):
         """ Configures IP routes between all nodes in the emulation topology. This is done in three
          steps:
@@ -75,6 +98,11 @@ class IPRoutingHelper(object):
 
         mini_nodes = net.hosts
         mini_links = net.links
+
+        # for node in mini_nodes:
+        #     print("[" + node.name + "]")
+        #     for intf in node.intfs:
+        #         print(node.intfs[intf].ip)
 
         # Enabling IP forwaring on all nodes
         info('Configure IP forwarding on all nodes\n')
@@ -96,21 +124,54 @@ class IPRoutingHelper(object):
             networkGraph.add_edges([(a, b), (b, a)])
 
         named_paths = []
+        existing_paths = {}
+        shortest_paths = []
+        #from_node = "no"
+        #to_node = "lt"
         for from_node in node_names:
             for to_node in node_names:
                 if from_node != to_node:
+                    if (from_node, to_node) in existing_paths or (to_node, from_node) in existing_paths:
+                        continue
                     paths = networkGraph.get_all_shortest_paths(from_node, to_node)
                     if len(paths) == 0:
                         continue
+                    # for p in paths:
+                    #     print([networkGraph.vs['name'][n] for n in p])
+                    # print("----")
+                    paths.sort(key=lambda x: str(x))
+                    paths.sort(key=lambda x: len(x))
+                    # for p in paths:
+                    #     print([networkGraph.vs['name'][n] for n in p])
+                    # print("----")
                     shortest_path = paths[0]
                     shortest_path_with_nodenames = []
                     for node in shortest_path:
                         shortest_path_with_nodenames.append(networkGraph.vs['name'][node])
-                    named_paths.append(shortest_path_with_nodenames)
+                    shortest_paths.append(shortest_path_with_nodenames)
+
+        shortest_paths.sort(key=lambda x: len(x), reverse=True)
+        for path in shortest_paths:
+            # Check if the beginning of the path already exist
+            starting_subpaths = [path[0:i] for i in range(3, len(path))]
+            starting_subpaths.reverse()
+            for starting_subpath in starting_subpaths:
+                if (starting_subpath[0], starting_subpath[-1]) in existing_paths:
+                    # if the beginnin of the path exist, do not create a new one
+                    existing = existing_paths[(starting_subpath[0], starting_subpath[-1])]
+                    path = existing[:] + path[len(existing) : ]
+                    break
+            # Create all subpaths of lengths 2 to len
+            subpaths = IPRoutingHelper.calculateAllSubPaths(path)
+            for subpath in subpaths:
+                if (subpath[0], subpath[-1]) not in existing_paths:
+                    named_paths.append(subpath)
+                    existing_paths[(subpath[0], subpath[-1])] = subpath
 
         # Iterate over all paths and configure the routes using the 'route add'
         info('Configure routes on all nodes\n')
         for path in named_paths:
+            print(path)
             start_node = path[0]
             end_node = path[-1]
             mini_start = net.get(start_node)
@@ -123,12 +184,12 @@ class IPRoutingHelper(object):
                 addr = mini_end.intfs[intf].ip
                 if len(path) == 2:
                     # For direct connection, configure exit interface
-                    info('[{}] route add -host {} dev {}\n'.format(start_node, addr, start_intf))
+                    # info('[{}] route add -host {} dev {}\n'.format(start_node, addr, start_intf))
                     mini_start.cmd('route add -host {} dev {}'.format(addr, start_intf))
                 elif len(path) > 2:
                     # For longer paths, configure next hop as gateway
                     gateway_ip = link_info.end_ip
-                    info('[{}] route add -host {} dev {} gw {}\n'
-                         .format(start_node, addr, start_intf, gateway_ip))
+                    # info('[{}] route add -host {} dev {} gw {}\n'
+                    #     .format(start_node, addr, start_intf, gateway_ip))
                     mini_start.cmd('route add -host {} dev {} gw {}'
                                    .format(addr, start_intf, gateway_ip))
