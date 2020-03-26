830157507e6819bef14c6d67c3df5c9d9deecceb
phmoll <philipp.moll@aau.at>
2020-03-26 11:41:14 +0100

    Improve script to draw comparison boxplots

--- scripts/Comparison.py
+++ scripts/Comparison.py
@@ -37,15 +37,35 @@ class Setting:
         self.runNo = int(folderName[4].replace("run", ""))
         self.trace = folderName[5]
 
+    def __str__(self):
+        return "#Servers: {}, Topology: {}, Protocol: {}, RUN: {}, Trace: {}".format(self.servers, self.topology,
+                                                                                     self.protocol, self.runNo,
+                                                                                     self.trace)
 
-def do_calculation(resultDir, facet):
-    directories = glob.glob(resultDir + "RESULTS*", recursive=True)
+
+def do_calculation(resultDir, facet, traceFilter, sizeFilter, protocolFilter, topoFilter):
+    directories = glob.glob(resultDir + "/RESULTS*", recursive=True)
 
     data = pd.DataFrame()
 
     for foldername in directories:
         setting = Setting(foldername)
-        settingData = pd.read_csv(foldername + "/testvalues.csv", sep='\t')
+        if len(glob.glob(foldername + "/RESULTS*.csv")) == 0:
+            continue
+        csvFile = glob.glob(foldername + "/RESULTS*.csv")[0]
+        print(setting)
+
+        if topoFilter != None and setting.topology != topoFilter:
+            continue
+        if traceFilter != None and setting.trace != traceFilter:
+            continue
+        if sizeFilter != None and setting.servers != sizeFilter:
+            continue
+        if protocolFilter != None and setting.protocol != protocolFilter:
+            continue
+        if setting.runNo != 0:
+            continue
+
         heading = ""
         if facet is EvaluationAttribute.TRACE:
             heading = setting.trace
@@ -53,7 +73,8 @@ def do_calculation(resultDir, facet):
             heading = setting.protocol
         elif facet is EvaluationAttribute.TOPOLOGY:
             heading = setting.topology
-        data[heading] = settingData["syncLatencies"]
+        settingData = pd.read_csv(csvFile, sep='\t')
+        data[heading] = settingData["syncLatency"]
 
     fig, ax = plt.subplots()
     ax.set_title(str(facet))
@@ -61,25 +82,39 @@ def do_calculation(resultDir, facet):
     boxplotData = []
     labels = []
     for col in data.columns:
-        boxplotData.append(data[col])
+        filtered = data[col].to_numpy()
+        filtered = filtered[~np.isnan(filtered)]
+        print(filtered)
+        boxplotData.append(filtered)
         labels.append(col)
     ax.boxplot(boxplotData, showfliers=False)
 
     plt.xticks(range(1, len(boxplotData) + 1), labels)
 
-    plt.savefig("test.pdf")
+    plt.savefig(resultDir + "/test.pdf")
 
     print(data)
 
+## Possible commands to use
+# python3 ~/Coding/SyncProtocols/mini-ndn/scripts/Comparison.py --result-dir=$(pwd) --facetingField=PROTOCOL --sizeFilter=4 --topoFilter=continent --traceFilter=ChunkChanges-concentratedcsv
+# python3 ~/Coding/SyncProtocols/mini-ndn/scripts/Comparison.py --result-dir=$(pwd) --facetingField=TRACE --protocolFilter=QuadTree --sizeFilter=4 --topoFilter=cluster
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--result-dir', dest='resultDir', default="/tmp/")
     parser.add_argument('--facetingField', dest='facetingField', type=EvaluationAttribute.from_string,
                         choices=list(EvaluationAttribute), default=EvaluationAttribute.TRACE)
+    parser.add_argument('--protocolFilter', dest='protocolFilter', default=None)
+    parser.add_argument('--sizeFilter', dest='sizeFilter', default=None, type=int)
+    parser.add_argument('--traceFilter', dest='traceFilter', default=None)
+    parser.add_argument('--topoFilter', dest='topoFilter', default=None)
 
     args = parser.parse_args()
     resultDir = args.resultDir
     facetingField = args.facetingField
+    traceFilter = args.traceFilter
+    sizeFilter = args.sizeFilter
+    protocolFilter = args.protocolFilter
+    topoFilter = args.topoFilter
 
-    do_calculation(resultDir, facetingField)
+    do_calculation(resultDir, facetingField, traceFilter, sizeFilter, protocolFilter, topoFilter)
