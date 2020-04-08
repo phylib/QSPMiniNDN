f7c09390d377b36828723f301a346899453376c1
selisak <Selina.Isak@aau.at>
2020-04-08 11:15:01 +0200

    Now the standard deviation is calculated based on the average per run

--- scripts/visuals.py
+++ scripts/visuals.py
@@ -29,37 +29,41 @@ class Visualizer:
         for file in self.files:
             file.close()
 
-    def getMeanAndStandardDeviation(self, protocol, criteria):
+    def getMeanPerRun(self, protocol, criteria, run):
         sync_latencies = []
         for file in self.files:
-           if protocol in file.name and criteria in file.name:
+            if protocol in file.name and criteria in file.name and run in file.name:
                 rows = csv.reader(file, delimiter='\t')
                 for row in rows:
                     value = row[len(row) - 1]
 
                     # check if value is integer or float
-                    if(re.fullmatch(r'[0-9]+(\.[0-9]+){0,1}', value)):
+                    if (re.fullmatch(r'[0-9]+(\.[0-9]+){0,1}', value)):
                         sync_latencies.append(float(value))
 
+        return numpy.mean(sync_latencies)
 
-        return [numpy.mean(sync_latencies), numpy.std(sync_latencies)]
 
     def plotGroup(self, criteria):
-
         means = []
         stds = []
         for protocol in self.protocols:
-            values = self.getMeanAndStandardDeviation(protocol, criteria)
-            print("Avg %s: %f" % (protocol, values[0]))
-            print("StD %s: %f" % (protocol, values[1]))
-            means.append(values[0])
-            stds.append(values[1])
+            runmeans = []
+            for i in range(self.runNumber):
+                mean = self.getMeanPerRun(protocol, criteria, str(i))
+                runmeans.append(mean)
+            means.append(numpy.mean(runmeans))
+            stds.append(numpy.std(runmeans))
+            print("Avg %s: %f" % (protocol, means[len(means)-1]))
+            print("StD %s: %f" % (protocol, stds[len(stds)-1]))
+
+
 
         self.closeAllFiles()
 
         x_pos = numpy.arange(len(self.protocols))
         figure, axis = plotter.subplots()
-        axis.bar(x_pos, means, yerr=stds, align="center", alpha=0.8, ecolor="black", capsize=10)
+        axis.bar(x_pos, means, yerr=stds, align="center", alpha=0.5, ecolor="black", capsize=10)
         axis.set_ylabel("Sync Latencies")
         axis.set_xticks(x_pos)
         axis.set_xticklabels(self.protocols)
@@ -69,9 +73,6 @@ class Visualizer:
 
         plotter.tight_layout()
         plotter.show()
-
-
-
 
 if __name__ == "__main__":
 
