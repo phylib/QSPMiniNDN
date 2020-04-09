605076c42afc84ef6e155046bda926dac1904a00
selisak <Selina.Isak@aau.at>
2020-04-09 10:14:56 +0200

    When plotting a single subplot the bars are not labeled --> using a legend; also ticks are removed and the axis.bar(..) method is extracted for easier readability'

--- scripts/visuals.py
+++ scripts/visuals.py
@@ -1,8 +1,6 @@
 import numpy
 import matplotlib.pyplot as plotter
-import csv
 from scripts.filefetcher import FileFetcher
-import re
 
 class Visualizer:
 
@@ -55,10 +53,11 @@ class Visualizer:
         x_pos = numpy.arange(len(self.protocols))
         colors = [(1, 1, 1, 1), (0.6, 0.6, 0.6, 1), (0.2, 0.2, 0.2, 1)]
         figure, axis = plotter.subplots()
-        axis.bar(x_pos, means, yerr=stds, ecolor="black", capsize=10, color = colors, edgecolor='black')
+        bar = self.getBar(axis, x_pos, means, 0.75, stds, "black", 10, colors, "black")
         axis.set_ylabel("Sync Latencies")
-        axis.set_xticks(x_pos)
-        axis.set_xticklabels(self.protocols)
+
+        self.removeTicks(showLabel=False)
+        axis.legend((bar[0],bar[1],bar[2]), self.protocols)
         axis.set_title("Sync Latencies of three different protocols")
         axis.set_xlabel(sublabel)
         axis.yaxis.grid(True)
@@ -94,11 +93,12 @@ class Visualizer:
         width = 0.25
         colors = [(1, 1, 1, 1), (0.6, 0.6, 0.6, 1), (0.2, 0.2, 0.2, 1)]
         figure, axis = plotter.subplots()
-        quadTree = axis.bar(x_pos, means[0], width, yerr=stds[0], ecolor="black", capsize=10, color = colors[0], edgecolor='black')
-        stateVector = axis.bar(x_pos+width, means[1], width,yerr=stds[1], ecolor="black", capsize=10, color = colors[1], edgecolor='black')
-        zmq = axis.bar(x_pos+2*width, means[2], width, yerr=stds[2], ecolor="black", capsize=10, color = colors[2], edgecolor='black')
 
+        quadTree = self.getBar(axis, x_pos, means[0], width, stds[0], "black", 10, colors[0], "black")
+        stateVector = self.getBar(axis, x_pos + width, means[1], width, stds[1], "black", 10, colors[1], "black")
+        zmq = self.getBar(axis, x_pos + 2*width, means[2], width, stds[2], "black", 10, colors[2], "black")
         axis.set_ylabel("Sync Latencies")
+        self.removeTicks(showLabel=True)
         axis.set_xticks(x_pos+width)
         axis.set_xticklabels(groups)
         axis.set_title("Sync Latencies of three different protocols\nusing two different topologies")
@@ -108,6 +108,17 @@ class Visualizer:
 
         plotter.tight_layout()
         plotter.show()
+
+    def getBar(self, axis, position, mean, width, error, errorcolor, capsize, color, edgecolor):
+        return axis.bar(position, mean, width, yerr=error, ecolor=errorcolor, capsize=capsize, color=color, edgecolor=edgecolor)
+
+    def removeTicks(self, showLabel):
+        plotter.tick_params(
+            axis='x',      # changes apply to the x-axis
+            which='both',  # both major and minor ticks are affected
+            bottom=False,  # ticks along the bottom edge are off
+            top=False,     # ticks along the top edge are off
+            labelbottom=showLabel)
 
 if __name__ == "__main__":
 
