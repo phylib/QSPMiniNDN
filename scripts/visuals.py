7cb939e733de5f088cb8e927d0680eff5c636be5
selisak <Selina.Isak@aau.at>
2020-04-14 14:44:34 +0200

    removed method for single-group plotting; instead making the method for mulitple-group plotting more flexible

--- scripts/visuals.py
+++ scripts/visuals.py
@@ -57,66 +57,6 @@ class Visualizer:
         return numpy.mean(sync_latencies)
 
 
-    def plotGroup(self, filterCriteria, barGroup, sublabel):
-        """
-        calculate the mean + standard deviation for each protocol
-        so that each protocol can be represented by exactly one bar in the diagram +
-        its according error bar
-        --> only one bar group is plotted, therefore
-            each protocol has only one mean/standard deviation
-            and is only represented once (one bar + error bar)
-
-        """
-        means = []
-        stds = []
-
-        # calculate the mean of each run per protocol
-        # and calculate the mean + standard deviation
-        # of each protocol based on the means per run
-        for protocol in self.protocols:
-            runmeans = []
-
-            # calculate the means per run of each protocol
-            for i in range(self.runNumber):
-                mean = self.getMeanPerRun(protocol, barGroup, filterCriteria, str(i))
-                runmeans.append(mean)
-
-            # get the mean + standard deviation of each protocol
-            # by using the means per run for the calculation
-            means.append(numpy.mean(runmeans))
-            stds.append(numpy.std(runmeans))
-
-            # print the calculated values for each protocol
-            # --> use the last element of the list
-            #     because we need the most recently added value
-            print("Avg %s: %f" % (protocol, means[len(means)-1]))
-            print("StD %s: %f" % (protocol, stds[len(stds)-1]))
-
-        # define the set of the x-positions and colors of each bar
-        x_pos = numpy.arange(len(self.protocols))
-        colors = [(1, 1, 1, 1), (0.6, 0.6, 0.6, 1), (0.2, 0.2, 0.2, 1)]
-        figure, axis = plotter.subplots()
-
-        # make the bars by using the defined positions and colors
-        # --> use the calculated mean for each protocol to define its height
-        #     and the standard deviation for drawing the error bar
-        bar = self.getBar(axis, x_pos, means, 0.75, stds, "black", 10, colors, "black")
-
-        # define the labels, legend and remove the ticks
-        axis.set_ylabel("Sync Latencies")
-        self.removeTicks(showLabel=False)
-        axis.legend((bar[0], bar[1], bar[2]), self.protocols)
-        axis.set_title("Sync Latencies of three different protocols")
-        axis.set_xlabel("Setting: " + sublabel)
-
-        # show a grid along the y-axis and put it behind the bars
-        axis.set_axisbelow(True)
-        axis.yaxis.grid(True)
-
-        # prevent overlapping of elements and show the plot
-        plotter.tight_layout()
-        plotter.show()
-
     def getProtocolData(self, protocol, filterCriteria, barGroups):
         """
         return the means + standard deviations per bar group
@@ -149,7 +89,9 @@ class Visualizer:
 
         return [means, stds]
 
-    def plotAll(self, filterCriteria, barGroups, sublabel):
+
+
+    def plotGroups(self, filterCriteria, barGroups, sublabel):
         """
         calculate the mean + standard deviation per protocol for each bar group
         that should be plotted;
@@ -173,20 +115,27 @@ class Visualizer:
 
         # define the starting position ( = position of first bar) for each bar group
         # as well as the width and color for the bars
+        # if only one bar group is plotted, add a space of 0.1 between the bars
         x_pos = numpy.arange(len(barGroups))
         width = 0.25
+        space = 0
+        if(len(barGroups) == 1):
+            space = 0.1
         colors = [(1, 1, 1, 1), (0.6, 0.6, 0.6, 1), (0.2, 0.2, 0.2, 1)]
         figure, axis = plotter.subplots()
 
         # define the bars for each protocol we want to represent in the different bar groups
         quadTree = self.getBar(axis, x_pos, means[0], width, stds[0], "black", 10, colors[0], "black")
-        stateVector = self.getBar(axis, x_pos + width, means[1], width, stds[1], "black", 10, colors[1], "black")
-        zmq = self.getBar(axis, x_pos + 2*width, means[2], width, stds[2], "black", 10, colors[2], "black")
+        stateVector = self.getBar(axis, x_pos + (width+space), means[1], width, stds[1], "black", 10, colors[1], "black")
+        zmq = self.getBar(axis, x_pos + 2 * (width+space), means[2], width, stds[2], "black", 10, colors[2], "black")
 
         # define the labels, legend and remove the ticks
         axis.set_ylabel("Sync Latencies")
-        self.removeTicks(showLabel=True)
-        axis.set_xticks(x_pos+width)
+        if(len(barGroups)>1):
+            self.removeTicks(showLabel=True)
+        else:
+            self.removeTicks(showLabel=False)
+        axis.set_xticks(x_pos + width)
         axis.set_xticklabels(labels)
         axis.set_xlabel("Setting: " + sublabel)
         axis.set_title("Sync Latencies of three different protocols")
@@ -220,13 +169,9 @@ class Visualizer:
 if __name__ == "__main__":
 
     visualizer = Visualizer()
-    #visualizer.plotAll(["16","concentrated"], ["continent", "cluster"], "16 servers and high client concentration")
-    #visualizer.plotAll([], ["4", "16"])
 
-    ''' Use the following statements to plot the data by ONE topology'''
-    visualizer.plotGroup(["16","concentrated"], "cluster", "cluster topology and high client concentration")
-    #visualizer.plotGroup(["16", "concentrated"], "continent", "concentrated continent")
-
-
+    #visualizer.plotGroups(["16", "concentrated"], ["continent", "cluster"], "16 servers and high client concentration")
+    #visualizer.plotGroups(["16", "concentrated"], ["cluster"], "cluster topology and high client concentration")
+    visualizer.plotGroups(["16", "concentrated"], ["continent"], "continent topology and high client concentration")
 
 
